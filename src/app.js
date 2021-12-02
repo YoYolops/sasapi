bbcd722b338b00d160bd031d16f6e116c8f14933
Yohan Lopes <yohanhxh@gmail.com>
2021-12-02 15:22:45 -0300

    config/ setting eslint configuration

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
-import express from "express";
-import cors from "cors";
-import routes from "./routes.js";
+import express from 'express';
+import cors from 'cors';
+import routes from './routes.js';
 
 const app = express();
 
-app.use( cors() )
-app.use( express.json() )
-app.use( routes );
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
 export default app;
--- test/factories/createUser.js
+++
@@ -1,9 +0,0 @@
-import faker from "faker";
-
-export function createUser() {
-    return {
-        name: faker.internet.userName(),
-        password: faker.internet.password(),
-        email: faker.internet.email()
-    }
-}
